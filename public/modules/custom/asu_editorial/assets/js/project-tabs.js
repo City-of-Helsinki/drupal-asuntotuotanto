cc2f6e0ef4a83b780b611a09f1b949232867d274
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-10-06 17:02:51 +0300

    Themed (draft) the project tabs on profile page.

--- public/modules/custom/asu_editorial/assets/js/project-tabs.js
+++ public/modules/custom/asu_editorial/assets/js/project-tabs.js
@@ -1,32 +1,32 @@
 (function($, Drupal) {
   Drupal.behaviors.projectTabs = {
     attach() {
-      let tabs = $('.project__tab a');
-      let projectList = $('.project__list');
+      const tabs = $(".projects__tab a");
+      const projectList = $(".projects__list");
 
-      tabs.on('click', function(e) {
+      tabs.on("click", function(e) {
         e.preventDefault();
 
         // Remove active status from tabs.
         tabs.each(function() {
-          $(this).attr('aria-selected', 'false');
-          $(this).removeClass('is-active');
+          $(this).attr("aria-selected", "false");
+          $(this).removeClass("is-active");
         });
 
         // Remove active status from lists.
         projectList.each(function() {
-          $(this).removeClass('is-active');
+          $(this).removeClass("is-active");
         });
 
         // Add active status to clicked tab.
-        $(this).attr('aria-selected','true');
-        $(this).addClass('is-active');
+        $(this).attr("aria-selected", "true");
+        $(this).addClass("is-active");
 
         // Add active status to clicked list.
-        let activeProjectList = $(e.target).data('project');
+        const activeProjectList = $(e.target).data("project");
         projectList.each(function() {
-          if ($(this).data('projectId') === activeProjectList) {
-            $(this).addClass('is-active');
+          if ($(this).data("projectId") === activeProjectList) {
+            $(this).addClass("is-active");
           }
         });
       });
