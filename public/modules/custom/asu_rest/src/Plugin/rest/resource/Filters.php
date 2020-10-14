c28b86b146c15e8da55015cb47feb1fdf1af80d9
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-10-14 08:54:26 +0300

    translated labels

--- public/modules/custom/asu_rest/src/Plugin/rest/resource/Filters.php
+++ public/modules/custom/asu_rest/src/Plugin/rest/resource/Filters.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\asu_rest\Plugin\rest\resource;
 
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Console\Command\Shared\TranslationTrait;
 use Drupal\rest\Plugin\ResourceBase;
-use Drupal\taxonomy\Entity\Term;
-use Drupal\taxonomy\TermStorage;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -23,14 +23,19 @@ use Symfony\Component\HttpFoundation\Request;
  * )
  */
 final class Filters extends ResourceBase {
+  use TranslationTrait;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger)
-  {
+  /**
+   * Constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
   }
 
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
-  {
+  /**
+   * Create.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return parent::create($container, $configuration, $plugin_id, $plugin_definition);
   }
 
@@ -44,76 +49,88 @@ final class Filters extends ResourceBase {
    *   The HTTP response object.
    */
   public function get(Request $request) {
-
     $currentLanguage = \Drupal::languageManager()->getCurrentLanguage();
 
+    // Taxonomy_id => elastic_index_id.
     $taxonomies_as_filters = [
       'building_type' => 'project_building_type',
       'district' => 'project_district',
       'new_development_status' => 'project_new_development_status',
-      'state_of_sale' => 'state_of_sale'
+      'state_of_sale' => 'state_of_sale',
     ];
 
-    $properties = [
+    $vocabularies = Vocabulary::loadMultiple();
+    $responseData = [];
+    foreach ($taxonomies_as_filters as $taxonomy_name => $elastic_index_name) {
+      $terms = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadTree($taxonomy_name, 0, NULL, TRUE);
+
+      $items = [];
+      foreach ($terms as $term) {
+        $items[] = $term->hasTranslation($currentLanguage->getId()) ?
+          $term->getTranslation($currentLanguage->getId())->getName() : $term->getName();
+      }
+
+      $vocabulary_name = $vocabularies[$terms[0]->bundle()]->get('name');
+      $index_data = [
+        'label' => $vocabulary_name,
+        'items' => $items,
+        'suffix' => NULL,
+      ];
+
+      $responseData[$elastic_index_name] = $index_data;
+    }
+
+    $responseData['properties'] = [
+      'label' => $this->t('Additional selections'),
+      'items' => $this->getProperties(),
+      'suffix' => NULL,
+    ];
+
+    $responseData['room_count'] = [
+      'label' => $this->t('Room count'),
+      'items' => $this->getRoomCount(),
+      'suffix' => $this->t('r'),
+    ];
+
+    $responseData['living_area'] = [
+      'items' => [
+        $this->t('At least'),
+        $this->t('At most'),
+      ],
+      'label' => $this->t('area, m2'),
+      'suffix' => 'm2',
+    ];
+
+    return new JsonResponse($responseData);
+  }
+
+  /**
+   * Get list of properties.
+   *
+   * @return array
+   */
+  protected function getProperties() {
+    return [
       'project_has_elevator',
       'project_has_sauna',
       'has_apartment_sauna',
       'has_terrace',
       'has_balcony',
-      'has_yard'
+      'has_yard',
     ];
+  }
 
-    #get taxonomies & terms
-
-    $vocabularies = \Drupal\taxonomy\Entity\Vocabulary::loadMultiple();
-    $data = [];
-    foreach ($taxonomies_as_filters as $machine_name => $index_name) {
-
-      $terms = \Drupal::entityTypeManager()
-        ->getStorage('taxonomy_term')
-        ->loadTree($machine_name, 0, NULL, TRUE);
-
-      $items = [];
-      foreach ($terms as $term) {
-        $vocabulary_name = $vocabularies[$term->bundle()]->get('name');
-        $items[] = $term->hasTranslation($currentLanguage->getId()) ?
-          $term->getTranslation($currentLanguage->getId())->getName() : $term->getName();
-      }
-
-      $index_data = [
-        'label' => $vocabulary_name,
-        'items' => $items,
-      ];
-
-      $data[$index_name] = $index_data;
-    }
-
-    $data['properties'] = [
-      'label' => 'Lisävalinnat',
-      'items' => $properties
-    ];
-
-    $data['room_count'] = [
-      'label' => 'Huoneita',
-      'items' => [
-        '1 h',
-        '2 h',
-        '3 h',
-        '4 h',
-        '5+ h'
-      ]
-    ];
-
-    $data['living_area'] = [
-      'items' => [
-        'Vähintään',
-        'Enintään'
-      ],
-      'label' => 'Pinta-ala, m2'
-    ];
-
-    return new Response(json_encode($data));
-
+  /**
+   * Get list of room counts.
+   *
+   * @return array
+   */
+  protected function getRoomCount() {
+    $count = array_map('strval', range(1, 4, 1));
+    $count[] = "5+";
+    return $count;
   }
 
 }
