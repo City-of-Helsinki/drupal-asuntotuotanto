3c080526dc7324bd2a6a2874c294f9fcf6fab894
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-10-29 17:25:50 +0200

    Themed Asuntotuotanto admin theme and fixed scss nesting with postcss nesting.

--- public/themes/custom/asuntotuotanto/postcss.config.js
+++ public/themes/custom/asuntotuotanto/postcss.config.js
@@ -8,6 +8,7 @@ module.exports = {
     "postcss-preset-env", // Convert modern CSS into something most browsers can understand.
     "postcss-extend", // Minimize the number of repeat selectors and rules you write in CSS.
     "postcss-nested", // Unwrap nested rules like how Sass does it.
+    "postcss-nesting", // Nest style rules inside each other, following the CSS Nesting specification.
     "postcss-combine-media-query", // Looks for equal media query rules and appends them combined.
     "postcss-strip-inline-comments", // Strip inline comments.
   ],
