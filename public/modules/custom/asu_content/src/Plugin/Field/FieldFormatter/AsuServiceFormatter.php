a796f72841b626a8cab4a838ba32260b35be0bc7
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-11-02 10:35:39 +0200

    updated field formatter

--- public/modules/custom/asu_content/src/Plugin/Field/FieldFormatter/AsuServiceFormatter.php
+++ public/modules/custom/asu_content/src/Plugin/Field/FieldFormatter/AsuServiceFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\asu_content\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Plugin implementation of the Tasklist formatter.
@@ -22,13 +23,14 @@ class AsuServiceFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-
-    $template = 'asdasd';
-
-    $element = [
-      '#type' => 'inline_template',
-      '#template' => $template,
-    ];
+    $element = [];
+    foreach($items as $delta => $item) {
+      if($term = Term::load($item->get('term_id')->getValue())){
+        $term_name = $term->getName();
+        $distance = $item->get('distance')->getValue();
+        $element[$delta] = ['#markup' => "$term_name $distance m"];
+      }
+    }
     return $element;
   }
 
