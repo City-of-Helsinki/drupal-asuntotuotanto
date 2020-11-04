3b5480f284f583638d150dbb331e79801d289979
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-11-04 12:41:00 +0200

    take state of sale from reverse entity

--- public/modules/custom/asu_content/src/Plugin/ComputedField/StateOfSale.php
+++ public/modules/custom/asu_content/src/Plugin/ComputedField/StateOfSale.php
@@ -6,6 +6,7 @@ use Drupal\computed_field_plugin\Traits\ComputedSingleItemTrait;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\DataDefinitionInterface;
 use Drupal\Core\TypedData\TypedDataInterface;
+use Drupal\node\Entity\Node;
 use Drupal\taxonomy\Entity\Term;
 
 /**
@@ -23,6 +24,13 @@ class StateOfSale extends FieldItemList {
   use ComputedSingleItemTrait;
 
   /**
+   * The reverse entity service.
+   *
+   * @var \Drupal\asu_content\CollectReverseEntity
+   */
+  protected $reverseEntities;
+
+  /**
    * Constructs a ApartmentHoldingType object.
    *
    * @param \Drupal\Core\TypedData\DataDefinitionInterface $definition
@@ -36,6 +44,7 @@ class StateOfSale extends FieldItemList {
    */
   public function __construct(DataDefinitionInterface $definition, $name = NULL, TypedDataInterface $parent = NULL) {
     parent::__construct($definition, $name, $parent);
+    $this->reverseEntities = \Drupal::service('asu_content.collect_reverse_entity');
   }
 
   /**
@@ -50,10 +59,23 @@ class StateOfSale extends FieldItemList {
   protected function singleComputeValue() {
     $current_entity = $this->getEntity();
 
+
+    $current_entity = $this->getEntity();
+    $reverse_references = $this->reverseEntities->getReverseReferences($current_entity);
     $value = FALSE;
-    $id = $current_entity->field_state_of_sale->target_id;
-    if ($id && $term = Term::load($id)) {
-      $value = $term->field_machine_readable_name->value;
+
+    foreach ($reverse_references as $reference) {
+      if (
+        !empty($reference) &&
+        $reference['referring_entity'] instanceof Node
+      ) {
+        $reverse_entity = $reference['referring_entity'];
+        $id = $reverse_entity->field_state_of_sale->target_id;
+        if ($id && $term = Term::load($id)) {
+          $value = $term->field_machine_readable_name->value;
+        }
+
+      }
     }
 
     // TODO: When displaying the field in twig add value&label through theme.
