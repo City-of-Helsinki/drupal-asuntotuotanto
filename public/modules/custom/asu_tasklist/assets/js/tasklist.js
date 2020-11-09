eec968feb0b91f84e89337b70aa7a62d2e748414
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-11-09 12:55:54 +0200

    code fixes

--- public/modules/custom/asu_tasklist/assets/js/tasklist.js
+++ public/modules/custom/asu_tasklist/assets/js/tasklist.js
@@ -3,26 +3,32 @@
  *
  * Script to Show/hide asu tasklist description field if checkbox has been checked.
  */
-(function($){
-  const tasklistWrapperClass = '.field--type-asu-tasklist';
-  $(tasklistWrapperClass).find('.asu_task_wrapper').each(function(index, taskWrapper){
-    const checkbox = $(taskWrapper).children().find('input[type="checkbox"]')
-    const description = $(taskWrapper).children().find('input[type="text"]')
+(function($) {
+  const tasklistWrapperClass = ".field--type-asu-tasklist";
+  $(tasklistWrapperClass)
+    .find(".asu_task_wrapper")
+    .each(function(index, taskWrapper) {
+      const checkbox = $(taskWrapper)
+        .children()
+        .find("input[type='checkbox']");
+      const description = $(taskWrapper)
+        .children()
+        .find("input[type='text']");
 
-    //set initial value
-    if(checkbox[0] && $(checkbox[0]).is(":checked")){
-      $(description).show();
-    } else {
-      $(description).hide();
-    }
-
-    // set eventlistener
-    $(checkbox[0]).click(function(event){
-      if($(event.target).is(":checked")){
+      // Set form initial state.
+      if (checkbox[0] && $(checkbox[0]).is(":checked")) {
         $(description).show();
       } else {
         $(description).hide();
       }
+
+      // Set eventlistener
+      $(checkbox[0]).click(function(event) {
+        if ($(event.target).is(":checked")) {
+          $(description).show();
+        } else {
+          $(description).hide();
+        }
+      });
     });
-  });
 })(jQuery);
