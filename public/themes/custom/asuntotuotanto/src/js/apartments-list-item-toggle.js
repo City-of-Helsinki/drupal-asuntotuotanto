10d1a3fed45e552549856ae06dcb5fb3c3bfd8b0
shamalainen <sebastian@hamse.fi>
2021-04-26 11:56:44 +0300

    ASU-315: Create desktop table styles

--- public/themes/custom/asuntotuotanto/src/js/apartments-list-item-toggle.js
+++ public/themes/custom/asuntotuotanto/src/js/apartments-list-item-toggle.js
@@ -1,9 +1,35 @@
 (($, Drupal) => {
   Drupal.behaviors.apartmentsListItemToggle = {
     attach: function attach() {
+      const apartmentsListingElement = document.getElementsByClassName(
+        "project__apartments"
+      );
       const buttonElements = document.getElementsByClassName(
         "project__apartments-item-button"
       );
+      let currentWindowWidth = window.innerWidth;
+
+      const handleWindowWidthChange = () => {
+        currentWindowWidth = window.innerWidth;
+
+        [...apartmentsListingElement].forEach((element) => {
+          if (element.classList.contains("project__apartments--mobile")) {
+            if (currentWindowWidth < 1248) {
+              element.setAttribute("aria-hidden", false);
+            } else {
+              element.setAttribute("aria-hidden", true);
+            }
+          }
+
+          if (element.classList.contains("project__apartments--desktop")) {
+            if (currentWindowWidth >= 1248) {
+              element.setAttribute("aria-hidden", false);
+            } else {
+              element.setAttribute("aria-hidden", true);
+            }
+          }
+        });
+      };
 
       const handleClick = ({ target }) => {
         const { nextElementSibling } = target;
@@ -23,6 +49,9 @@
       [...buttonElements].forEach((button) =>
         button.addEventListener("click", handleClick)
       );
+
+      handleWindowWidthChange();
+      window.addEventListener("resize", handleWindowWidthChange);
     },
   };
 })(jQuery, Drupal);
