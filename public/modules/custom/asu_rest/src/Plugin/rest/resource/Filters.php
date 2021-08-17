0a5b24190659a9199385c6815eaedc3e2a650471
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-08-17 08:41:31 +0300

    added theme files

---
+++ public/modules/custom/asu_rest/src/Plugin/rest/resource/Filters.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Drupal\asu_rest\Plugin\rest\resource;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\rest\Plugin\ResourceBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Provides a resource to get available filters.
+ *
+ * @RestResource(
+ *   id = "filter_rest_resource",
+ *   label = @Translation("Filters"),
+ *   uri_paths = {
+ *     "canonical" = "/filters",
+ *     "https://www.drupal.org/link-relations/create" = "/filters"
+ *   }
+ * )
+ */
+final class Filters extends ResourceBase {
+  use StringTranslationTrait;
+
+  /**
+   * Responds to GET requests.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The HTTP response object.
+   */
+  public function get(Request $request) {
+    $currentLanguage = \Drupal::languageManager()->getCurrentLanguage();
+    $config = \Drupal::config('asu_rest.filters');
+    $filters = $config->get('filters');
+    $vocabularies = Vocabulary::loadMultiple();
+    $responseData = [];
+
+    foreach ($filters['taxonomy'] as $taxonomy_name => $elastic_index_name) {
+      $terms = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadTree($taxonomy_name, 0, NULL, TRUE);
+
+      if (!$terms) {
+        continue;
+      }
+
+      $items = [];
+
+      if ($taxonomy_name == 'districts') {
+        $projects = \Drupal::entityTypeManager()
+          ->getStorage('node')
+          ->loadByProperties(['type' => 'project']);
+
+        $items = [
+          'Hitas' => [],
+          'Haso' => [],
+        ];
+
+        // Get all unique districts separately for both ownership types.
+        foreach ($projects as $project) {
+          if (!$project->field_ownership_type->first()) {
+            continue;
+          }
+          if (!$ownership = $project->field_ownership_type->first()->entity->getName()) {
+            continue;
+          }
+          $ownership = $ownership == 'Haso' ? 'Haso' : 'Hitas';
+          $district = $project->field_district->first()->entity;
+
+          $name = $district->hasTranslation($currentLanguage->getId()) ?
+            $district->getTranslation($currentLanguage->getId())->getName() : $district->getName();
+          if (!array_search($name, $items[$ownership])) {
+            $items[strtolower('project_district_' . $ownership)][] = $name;
+          }
+        }
+
+        $vocabulary_name = $vocabularies[$terms[0]->bundle()]->get('name');
+        $index_hitas = [
+          'label' => $vocabulary_name,
+          'items' => isset($items['project_district_hitas']) ? array_unique($items['project_district_hitas']) : [],
+          'suffix' => NULL,
+        ];
+        $responseData[strtolower('project_district_hitas')] = $index_hitas;
+
+        $vocabulary_name = $vocabularies[$terms[0]->bundle()]->get('name');
+        $index_haso = [
+          'label' => $vocabulary_name,
+          'items' => isset($items['project_district_haso']) ? array_unique($items['project_district_haso']) : [],
+          'suffix' => NULL,
+        ];
+
+        $responseData[strtolower('project_district_haso')] = $index_haso;
+
+      }
+      else {
+        foreach ($terms as $term) {
+          $items[] = $term->hasTranslation($currentLanguage->getId()) ?
+            $term->getTranslation($currentLanguage->getId())->getName() : $term->getName();
+        }
+
+        $vocabulary_name = $vocabularies[$terms[0]->bundle()]->get('name');
+        $index_data = [
+          'label' => $vocabulary_name,
+          'items' => $items,
+          'suffix' => NULL,
+        ];
+
+        $responseData[$elastic_index_name] = $index_data;
+
+      }
+
+    }
+
+    foreach ($filters['taxonomy_machinename'] as $taxonomy_name => $elastic_index_name) {
+      /** @var \Drupal\config_terms\TermStorageInterface $term_storage */
+      $term_storage = \Drupal::entityTypeManager()
+        ->getStorage('config_terms_term');
+      $terms = $term_storage->loadTree($taxonomy_name);
+
+      if (!$terms) {
+        continue;
+      }
+
+      $items = [];
+      foreach ($terms as $term) {
+        $items[] = $term->id();
+      }
+
+      $index_data = [
+        'label' => $this->t('State of sale'),
+        'items' => $items,
+        'suffix' => NULL,
+      ];
+
+      $responseData[$elastic_index_name] = $index_data;
+    }
+
+    $responseData['properties'] = [
+      'label' => $this->t('Additional selections'),
+      'items' => $this->getProperties(),
+      'suffix' => NULL,
+    ];
+
+    $responseData['room_count'] = [
+      'label' => $this->t('Room count'),
+      'items' => $this->getRoomCount(),
+      'suffix' => $this->t('rooms'),
+    ];
+
+    $responseData['living_area'] = [
+      'items' => [
+        $this->t('At least'),
+        $this->t('At most'),
+      ],
+      'label' => $this->t('area, m2'),
+      'suffix' => 'm2',
+    ];
+
+    $responseData['debt_free_sales_price'] = [
+      'items' => [
+        $this->t('Price at most'),
+      ],
+      'label' => $this->t('Price at most'),
+      'suffix' => '€',
+    ];
+
+    return new JsonResponse($responseData);
+  }
+
+  /**
+   * Get list of properties.
+   *
+   * @return array
+   *   Array of apartment and room properties
+   */
+  protected function getProperties() {
+    return [
+      'project_has_elevator',
+      'project_has_sauna',
+      'has_apartment_sauna',
+      'has_terrace',
+      'has_balcony',
+      'has_yard',
+    ];
+  }
+
+  /**
+   * Get list of room counts.
+   *
+   * @return array
+   *   Array of room counts
+   */
+  protected function getRoomCount() {
+    $count = array_map('strval', range(1, 4, 1));
+    $count[] = "5+";
+    return $count;
+  }
+}
---
+++ public/themes/custom/public/asuntotuotanto/src/js/common.js
@@ -0,0 +1,204 @@
+((Drupal, $) => {
+  Drupal.behaviors.headerDesktopSubmenu = {
+    attach: function attach() {
+      const headerElement = document.getElementsByClassName("page-header")[0];
+      const mainMenuDesktopItemElements = document
+        .getElementsByClassName("menu--main-menu is-desktop")[0]
+        .getElementsByClassName("menu__item");
+      const currentWindowWidth = window.innerWidth;
+
+      if (mainMenuDesktopItemElements) {
+        const hasActiveHeaderSubmenu = [
+          ...mainMenuDesktopItemElements,
+        ].some((item) => item.classList.contains("is-active"));
+
+        if (hasActiveHeaderSubmenu && currentWindowWidth >= 992) {
+          headerElement.classList.add("has-submenu");
+        }
+      }
+    },
+  };
+
+  Drupal.behaviors.mobileMenuToggle = {
+    attach: function attach() {
+      const bodyElement = document.getElementsByTagName("body")[0];
+      const headerBackgroundShadowElement = document.getElementsByClassName(
+        "page-header__background-shadow"
+      )[0];
+
+      const mobileNavigationToggleButtonElement = document.getElementById(
+        "mobile_navigation_toggle_button"
+      );
+
+      const mobileNavigationElement = document.getElementsByClassName(
+        "page-header__bottom"
+      )[0];
+
+      const mobileSubmenuButtonElements = document.getElementsByClassName(
+        "sub-menu__button"
+      );
+
+      const handleMobileNavigationClose = () => {
+        mobileNavigationElement.setAttribute("aria-hidden", true);
+        mobileNavigationToggleButtonElement.setAttribute(
+          "aria-expanded",
+          false
+        );
+        bodyElement.style.overflow = "visible";
+        mobileNavigationElement.classList.add("is-hidden");
+      };
+
+      const handleMobileNavigationToggleClick = ({ target }) => {
+        if (mobileNavigationElement.classList.contains("is-hidden")) {
+          mobileNavigationElement.setAttribute("aria-hidden", false);
+          target.setAttribute("aria-expanded", true);
+          bodyElement.style.overflow = "hidden";
+        } else {
+          mobileNavigationElement.setAttribute("aria-hidden", true);
+          target.setAttribute("aria-expanded", false);
+          bodyElement.style.overflow = "visible";
+        }
+
+        mobileNavigationElement.classList.toggle("is-hidden");
+      };
+
+      const handleSubmenuToggleClick = ({ target }) => {
+        [...mobileSubmenuButtonElements].forEach((element) => {
+          if (element !== target) {
+            element.parentElement.nextElementSibling.classList.add("is-hidden");
+            element.parentElement.nextElementSibling.setAttribute(
+              "aria-hidden",
+              true
+            );
+            element.setAttribute("aria-expanded", false);
+          }
+        });
+
+        const submenuElement = target.parentElement.nextElementSibling;
+
+        if (target.getAttribute("aria-expanded") === "false") {
+          submenuElement.classList.remove("is-hidden");
+          submenuElement.setAttribute("aria-hidden", false);
+          target.setAttribute("aria-expanded", true);
+        } else {
+          submenuElement.classList.add("is-hidden");
+          submenuElement.setAttribute("aria-hidden", true);
+          target.setAttribute("aria-expanded", false);
+        }
+      };
+
+      let currentWindowWidth = window.innerWidth;
+
+      if (currentWindowWidth > 992) {
+        mobileNavigationToggleButtonElement.setAttribute("aria-hidden", true);
+      } else {
+        mobileNavigationToggleButtonElement.setAttribute("aria-hidden", false);
+      }
+
+      window.addEventListener("resize", () => {
+        currentWindowWidth = window.innerWidth;
+
+        if (currentWindowWidth > 992) {
+          mobileNavigationToggleButtonElement.setAttribute("aria-hidden", true);
+          handleMobileNavigationClose();
+        } else {
+          mobileNavigationToggleButtonElement.setAttribute(
+            "aria-hidden",
+            false
+          );
+        }
+      });
+
+      mobileNavigationToggleButtonElement.addEventListener(
+        "click",
+        handleMobileNavigationToggleClick
+      );
+
+      headerBackgroundShadowElement.addEventListener(
+        "click",
+        handleMobileNavigationClose
+      );
+
+      if (mobileSubmenuButtonElements) {
+        [...mobileSubmenuButtonElements].forEach((element) => {
+          element.addEventListener("click", handleSubmenuToggleClick);
+        });
+      }
+    },
+  };
+
+  Drupal.behaviors.languageSwitcher = {
+    attach: function attach(context) {
+      const languageSwitcherToggleButton = $(".lang-switcher__button", context);
+      const languageSwitcherWrapper = $(".lang-switcher__dropdown", context);
+
+      let currentWindowWidth = window.innerWidth;
+
+      if (currentWindowWidth > 992) {
+        languageSwitcherWrapper.attr("aria-hidden", "true");
+        languageSwitcherToggleButton.attr("aria-expanded", "false");
+        languageSwitcherToggleButton.attr("aria-hidden", "false");
+      } else {
+        languageSwitcherWrapper.attr("aria-hidden", "false");
+        languageSwitcherToggleButton.attr("aria-hidden", "true");
+      }
+
+      window.addEventListener("resize", () => {
+        currentWindowWidth = window.innerWidth;
+
+        if (currentWindowWidth > 992) {
+          languageSwitcherWrapper.attr("aria-hidden", "true");
+          languageSwitcherToggleButton.attr("aria-expanded", "false");
+          languageSwitcherToggleButton.attr("aria-hidden", "false");
+        } else {
+          languageSwitcherWrapper.attr("aria-hidden", "false");
+          languageSwitcherToggleButton.attr("aria-hidden", "true");
+        }
+      });
+
+      const outsideClickListener = function outsideClickListener(event) {
+        const target = $(event.target);
+
+        if (
+          !target.closest(".lang-switcher__dropdown").length &&
+          $(".lang-switcher__dropdown").is(":visible")
+        ) {
+          // eslint-disable-next-line no-use-before-define
+          handleInteraction(event);
+          // eslint-disable-next-line no-use-before-define
+          removeClickListener();
+        }
+      };
+
+      const removeClickListener = function removeClickListener() {
+        document.removeEventListener("click", outsideClickListener);
+      };
+
+      function handleInteraction(e) {
+        e.stopImmediatePropagation();
+
+        if (languageSwitcherWrapper.attr("aria-hidden") === "false") {
+          languageSwitcherWrapper.attr("aria-hidden", "true");
+          languageSwitcherToggleButton.attr("aria-expanded", "false");
+        } else {
+          languageSwitcherWrapper.attr("aria-hidden", "false");
+          languageSwitcherToggleButton.attr("aria-expanded", "true");
+          document.addEventListener("click", outsideClickListener);
+        }
+      }
+
+      languageSwitcherToggleButton.on({
+        click: function touchstartclick(e) {
+          handleInteraction(e);
+        },
+        keydown: function keydown(e) {
+          if (e.which === 27) {
+            languageSwitcherWrapper.attr("aria-hidden", "true");
+            languageSwitcherToggleButton.attr("aria-expanded", "false");
+            removeClickListener();
+          }
+        },
+      });
+    },
+  };
+})(Drupal, jQuery);
