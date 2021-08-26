1728afd0dec21513080755b9a7f1f3df2267b917
shamalainen <sebastian@hamse.fi>
2021-08-26 10:43:13 +0300

    ASU-983: Create form page to delete all test users

---
+++ public/modules/custom/asu_user/src/DeleteTestUsers.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\asu_user;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Class DeleteTestUsers.
+ */
+class DeleteTestUsers {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs an object.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  public function doDeleteTestUsers() {
+    $user_ids = \Drupal::entityQuery('user')->execute();
+    $users = \Drupal\user\Entity\User::loadMultiple($user_ids);
+
+    foreach ($users as $user) {
+      if (str_starts_with($user->getAccountName(), 'test_')) {
+        $user->delete();
+      }
+    }
+  }
+
+}
