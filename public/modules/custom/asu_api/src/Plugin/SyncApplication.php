f3c63c9d7f6922fadb83124d0986e3bad7beb132
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-11-09 09:40:15 +0200

    application queue worker

---
+++ public/modules/custom/asu_api/src/Plugin/SyncApplication.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\asu_api\Plugin\QueueWorker;
+
+use Drupal\asu_api\Api\BackendApi\Request\CreateApplicationRequest;
+use Drupal\asu_api\ApiManager;
+use Drupal\asu_application\Entity\Application;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Processes failed application requests.
+ *
+ * @QueueWorker(
+ *   id = "application_api_queue",
+ *   title = @Translation("Sync order to backend api."),
+ *   cron = {"time" = 60}
+ * )
+ */
+class SyncApplication extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  private ApiManager $apiManager;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ApiManager $apiManager)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->apiManager = $apiManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('asu_api.apimanager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data)
+  {
+    $application = Application::load($data);
+    try {
+      /** @var Node $project */
+      $project = Node::load($application->getProjectId());
+
+      /** @var Node[] $apartments */
+      $apartments = Node::loadMultiple($application->getApartmentIds());
+      $apartmentData = [];
+      foreach($apartments as $apartment) {
+        $apartmentData[$apartment->id()] = $apartment->uuid();
+      }
+
+      $projectData = [
+        'uuid' => $project->uuid(),
+        'apartment_uuids' => $apartmentData
+      ];
+      $request = new CreateApplicationRequest($application->getOwner(), $application, $projectData);
+      $this->apiManager->handleBackendRequest($request);
+    }
+    catch(\Exception $e) {
+      // @todo: Logger should maybe log about this particular application.
+      throw $e;
+    }
+  }
+}
