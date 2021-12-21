e676cfd9c51377d023acbb3862cbc62e50f64c0d
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-12-21 15:36:43 +0200

    ASU-980: frontend changes

--- public/modules/custom/asu_application/assets/js/application-results.js
+++ public/modules/custom/asu_application/assets/js/application-results.js
@@ -1,22 +1,39 @@
-((Drupal) => {
+((Drupal,$) => {
   Drupal.behaviors.applicantFormToggle = {
-    attach: function attach() {
-
+    attach: function attach(context, settings) {
       const elements = document.getElementsByClassName('asu-results');
       Array.from(elements).forEach(function(element, index, array) {
         element.addEventListener('click', (event) => {
-          console.log('click');
-          jQuery.ajax({
-            url: "call/ajax/application/results",
-            method :'GET',
-            dataType: "json",
-            success: function(result){
-              $a(".region-content").html(result.html);
-            }
-          })
-        })
-      })
-
+          const element = $(event.target)
+          if (element.data('loaded') == 1) {
+            return;
+          }
+          const id = element.data('application');
+          $.ajax({
+            url: "application/results",
+            method : 'POST',
+            dataType: 'json',
+            data: {
+              'application_id': id
+            },
+            success: function(results){
+              element.data('loaded', 1);
+              // Update elements
+              Array.from(results[0]).forEach(function(apartment_result, index, array) {
+                let apartment_class = '.application-apartment-' + apartment_result.apartment_id;
+                Array.from($('.lottery-result' + apartment_class)).forEach(function(result_row) {
+                  $(result_row).find('.result').first().html(apartment_result.lottery_position);
+                  $(result_row).find('.current-position').first().html(apartment_result.position);
+                  $(result_row).find('.status').first().html(apartment_result.status);
+                });
+              });
+            },
+            failed: function(result){
+              // Do nothing.
+            },
+          });
+        });
+      });
     },
   };
-})(Drupal);
+})(Drupal, jQuery);
