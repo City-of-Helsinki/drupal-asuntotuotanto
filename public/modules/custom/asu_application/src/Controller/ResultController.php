b706eacb071a04b264a7d535c583c1dfd24ebb0e
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-01-12 13:37:55 +0200

    ASU-980: code fixes

--- public/modules/custom/asu_application/src/Controller/ResultController.php
+++ public/modules/custom/asu_application/src/Controller/ResultController.php
@@ -17,12 +17,10 @@ class ResultController extends ControllerBase {
   /**
    * Get apartment result array.
    */
-  public function getResults() {
+  public function getResults(): AjaxResponse {
     $user = User::load(\Drupal::currentUser()->id());
     $applicationId = \Drupal::request()->get('application_id');
     if ($user && $applicationId) {
-
-
       $backendApi = \Drupal::service('asu_api.backendapi');
       $application = Application::load($applicationId);
 
@@ -32,38 +30,32 @@ class ResultController extends ControllerBase {
       }
 
       try {
+        $request = new ApplicationLotteryResult($user->uuid(), $project->uuid());
+        $request->setSender($user);
 
-        try {
-          $request = new ApplicationLotteryResult($user->uuid(), $project->uuid());
-          $request->setSender($user);
-
-          /** @var \Drupal\asu_api\Api\BackendApi\Request\ApplicationLotteryResultResponse $response */
-          $responseContent = $backendApi
-            ->send($request)
-            ->getContent();
-          $results = [];
-        }
-        catch(Exception $e){
-          $this->getLogger('asu_api')->critical('Exception when customer tried to access his application results: ' . $e->getMessage());
-        }
-
-        foreach ($responseContent as $result) {
-          $apartment = \Drupal::service('entity.repository')->loadEntityByUuid('node', $result['apartment_uuid']);
-          $results[] = [
-            'apartment_id' => $apartment->id(),
-            'apartment_uuid' => $result['apartment_uuid'],
-            'apartment' => $apartment->field_apartment_number->value,
-            'position' => $result['lottery_position'],
-            'current_position' => $result['queue_position'],
-            'status' => t($result['status'])
-          ];
-        }
-
-        return new AjaxResponse($results);
+        /** @var \Drupal\asu_api\Api\BackendApi\Request\ApplicationLotteryResultResponse $response */
+        $responseContent = $backendApi
+          ->send($request)
+          ->getContent();
       }
-      catch (\Exception $e) {
+      catch (Exception $e) {
+        $this->getLogger('asu_api')->critical('Exception when customer tried to access his application results: ' . $e->getMessage());
         return new AjaxResponse(400, []);
       }
+
+      $results = [];
+      foreach ($responseContent as $result) {
+        $apartment = \Drupal::service('entity.repository')->loadEntityByUuid('node', $result['apartment_uuid']);
+        $results[] = [
+          'apartment_id' => $apartment->id(),
+          'apartment_uuid' => $result['apartment_uuid'],
+          'apartment' => $apartment->field_apartment_number->value,
+          'position' => $result['lottery_position'],
+          'current_position' => $result['queue_position'],
+          'status' => t($result['status']),
+        ];
+      }
+      return new AjaxResponse($results);
     }
     return new AjaxResponse([], 400);
   }
