39d777759b081228a1533c85c47b5fd9b4bb04ad
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-01-13 09:59:32 +0200

    new exceptions

---
+++ public/modules/custom/asu_api/src/Exception/IllegalApplicationException.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\asu_api\Exception;
+
+/**
+ * Exception thrown when response is 4xx.
+ *
+ * Class RequestException.
+ *
+ * @package Drupal\asu_api\Exception
+ */
+class IllegalApplicationException extends \Exception {
+
+  /**
+   * Error code mentioned in the response message.
+   *
+   * @var string
+   */
+  protected string $apiErrorCode;
+
+  /**
+   * Constructor.
+   */
+  public function __construct($message = "", $code = 0, $previous = NULL) {
+    $this->apiErrorCode = '';
+    if ($message && $code = $this->parseApiErrorCode($message)) {
+      $this->apiErrorCode = $code;
+    }
+    parent::__construct($message, $code, $previous);
+  }
+
+  /**
+   * Return api error code.
+   */
+  public function getApiErrorCode(): string {
+    return $this->apiErrorCode;
+  }
+
+  /**
+   * Custom string representation of object.
+   */
+  public function __toString() {
+    return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+  }
+
+  /**
+   * Parse api error code from the beginning of the message.
+   *
+   * @param string $message
+   *   Response message.
+   *
+   * @return string
+   *   Error code.
+   */
+  private function parseApiErrorCode(string $message): string {
+    $matches = [];
+    preg_match('(\d+)', $message, $matches);
+    if (!isset($matches[0])) {
+      return FALSE;
+    }
+    if ((strpos($message, $matches[0]) === 0)) {
+      return (string) $matches[0];
+    }
+  }
+
+}
