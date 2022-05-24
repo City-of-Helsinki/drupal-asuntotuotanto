10040269d86b70dbd93a5762efd9e355b780a9c5
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-05-24 07:52:49 +0300

    ASU-1471: command to run user migrations

---
+++ public/modules/custom/asu_migration/src/Commands/MigrateUserCsvCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\asu_migration\Commands;
+
+use Drupal\asu_migration\UserMigrationService;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A drush command file.
+ */
+class MigrateUserCsvCommand extends DrushCommands {
+
+  public function __construct(private UserMigrationService $migration)
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Drush command that migrates users from file.
+   *
+   * @command asu:user-migration
+   */
+  public function migrateUsers() {
+    $errors = $this->migration->migrate();
+    foreach($errors as $key => $error) {
+      $this->output()->writeln("$key: $error");
+    }
+  }
+
+}
