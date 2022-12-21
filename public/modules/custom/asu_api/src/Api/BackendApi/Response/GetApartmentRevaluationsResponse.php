e4ea0c593d638645da7fa31033ae4ecd6a7e5756
Tuomas Välimaa <tuomas.valimaa@druid.fi>
2022-12-21 15:54:42 +0200

    ASU-1650: apartment revaluation api

---
+++ public/modules/custom/asu_api/src/Api/BackendApi/Response/GetApartmentRevaluationsResponse.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\asu_api\Api\BackendApi\Response;
+
+use Drupal\asu_api\Api\Response;
+use Drupal\asu_api\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Response for user creation request.
+ */
+class GetApartmentRevaluationsResponse extends Response {
+
+  /**
+   * Result of the request.
+   *
+   * @var array
+   */
+  private array $content;
+
+  /**
+   * Constructor.
+   *
+   * @param array $content
+   *   Contents of the response.
+   */
+  public function __construct(array $content) {
+    $this->content = $content;
+  }
+
+  /**
+   * Get profile id returned by create user request.
+   *
+   * @return string
+   *   Profile id in authentication request.
+   */
+  public function getContent(): array {
+    return $this->content;
+  }
+
+  /**
+   * Create new user response from http response.
+   *
+   * @param \Psr\Http\Message\ResponseInterface $response
+   *   Guzzle response.
+   *
+   * @return GetApartmentRevaluationsResponse
+   *   CreateUserResponse.
+   *
+   * @throws \Exception
+   */
+  public static function createFromHttpResponse(ResponseInterface $response): GetApartmentRevaluationsResponse {
+    if (!self::requestOk($response)) {
+      throw new RequestException('Bad status code: ' . $response->getStatusCode());
+    }
+    $content = json_decode($response->getBody()->getContents(), TRUE);
+    return new self($content);
+  }
+
+}
