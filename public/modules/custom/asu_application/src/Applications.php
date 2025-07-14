a33baeef532bc9d2d6e84fcc5926793e43d58450
Andrey Baklanov <andrey.baklanov@twoday.com>
2025-07-14 15:14:57 +0300

    ASU-1736 Fix phpcs errors

--- public/modules/custom/asu_application/src/Applications.php
+++ public/modules/custom/asu_application/src/Applications.php
@@ -107,8 +107,8 @@ class Applications {
       $applicationId = $application->id();
       if ($projectId && $applicationId) {
         $result[] = [
-          'project_id' => (int)$projectId,
-          'application_id' => (int)$applicationId,
+          'project_id' => (int) $projectId,
+          'application_id' => (int) $applicationId,
         ];
       }
     }
