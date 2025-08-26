692efa27db120a2ebdde7b360c4028f6b57fa520
leevi-identio <leevi@identio.fi>
2025-08-26 12:02:34 +0300

    ASU-1712: Allow applying to HASO apartments after application period has ended - Add field `can_apply_afterwards`
    - Add field `project_can_apply_afterwards` to EL index

--- public/modules/custom/asu_application/src/Form/ApplicationForm.php
+++ public/modules/custom/asu_application/src/Form/ApplicationForm.php
@@ -268,8 +268,12 @@ HTML;
         return new RedirectResponse($applicationsUrl);
       }
 
-      if ($this->isApplicationPeriod('after', $startDate, $endDate)) {
-        $this->messenger()->addMessage($this->t('The application period has ended. You can still apply for the apartment by contacting the responsible salesperson.'));
+      if (
+        strtolower($project_data['ownership_type']) != 'haso' &&
+        $this->isApplicationPeriod('after', $startDate, $endDate)
+      ) {
+        // $this->messenger()->addMessage($this->t('The application period has ended. You can still apply for the apartment by contacting the responsible salesperson.'));
+        $this->messenger()->addMessage('ownership_type is: '.$project_data["ownership_type"]);
         $freeApplicationUrl = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() .
           '/contact/apply_for_free_apartment?project=' . $project_id;
         return new RedirectResponse($freeApplicationUrl);
