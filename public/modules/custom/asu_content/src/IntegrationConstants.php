9c0d8ad4ab15fc554100d9c3b8a4a3681e25668c
leevi-identio <leevi@identio.fi>
2026-01-21 15:26:36 +0200

    ASU-1827: Fix some lint errs

--- public/modules/custom/asu_content/src/IntegrationConstants.php
+++ public/modules/custom/asu_content/src/IntegrationConstants.php
@@ -2,29 +2,44 @@
 
 namespace Drupal\asu_content;
 
-use Drupal\Core\Entity\EntityInterface;
-
+/**
+ * Integration constants.
+ *
+ * Class for storing lists of mandatory fields for integrations.
+ */
 class IntegrationConstants {
+  /**
+   * Integration triggers.
+   */
   public const INTEGRATION_TRIGGERS = [
     'field_publish_on_oikotie' => self::OIKOTIE_REQUIRED_FIELDS_APARTMENT,
     'field_publish_on_etuovi' => self::ETUOVI_REQUIRED_FIELDS_APARTMENT,
   ];
 
-  public static function getRequiredFieldsForIntegration($integration_trigger, string $ownership_type) {
-    $required_fields = self::INTEGRATION_TRIGGERS[$integration_trigger];    
+  /**
+   * Get required fields for integration.
+   *
+   * @param string $integration_trigger
+   *   The field machine name of the integration boolean.
+   * @param string $ownership_type
+   *   The ownership type.
+   *
+   * @return array
+   *   An array of required fields for the integration.
+   */
+  public static function getRequiredFieldsForIntegration(string $integration_trigger, string $ownership_type) {
+    $required_fields = self::INTEGRATION_TRIGGERS[$integration_trigger];
     $ownership_type_fields = match (strtolower($ownership_type)) {
       'hitas' => self::HITAS_PRICE_FIELDS,
       'haso' => self::HASO_PRICE_FIELDS,
       default => [],
     };
     $required_fields = array_merge($required_fields, $ownership_type_fields);
-    \Drupal::logger('asu_content')->notice('getRequiredFieldsForIntegration Required fields: @required_fields, ownership type: @ownership_type', ['@required_fields' => $required_fields, '@ownership_type' => $ownership_type]);
     return $required_fields;
   }
 
-  
   public const ETUOVI_REQUIRED_FIELDS_APARTMENT = [
-    'field_room_count'
+    'field_room_count',
   ];
 
   public const OIKOTIE_REQUIRED_FIELDS_APARTMENT = [
