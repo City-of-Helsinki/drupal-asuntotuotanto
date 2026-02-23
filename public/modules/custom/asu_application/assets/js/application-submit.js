bbfa6aeba0c9be66cc250da66dcfa689bd3a12ed
Andrey Baklanov <andrey.baklanov@twoday.com>
2026-02-23 12:33:05 +0200

    ASU-1807: update styled mechanism for dialog

--- public/modules/custom/asu_application/assets/js/application-submit.js
+++ public/modules/custom/asu_application/assets/js/application-submit.js
@@ -1,4 +1,13 @@
 (function ($, Drupal, once) {
+  const decorateDialogButtons = function (dialogElement) {
+    const $buttons = $(dialogElement)
+      .closest(".ui-dialog")
+      .find(".ui-dialog-buttonset button");
+
+    $buttons.eq(0).addClass("hds-button hds-button--primary");
+    $buttons.eq(1).addClass("hds-button hds-button--secondary");
+  };
+
   Drupal.behaviors.applicationSubmit = {
     attach: function (context, settings) {
       if (!settings.asuApplication?.hasExistingApplication) {
@@ -34,6 +43,10 @@
             $dialog.dialog({
               modal: true,
               width: 450,
+              dialogClass: "asu-application-confirm-dialog",
+              open: function () {
+                decorateDialogButtons(this);
+              },
               buttons: {
                 [continueLabel]: function () {
                   $confirmInput.val("1");
@@ -58,6 +71,10 @@
             $dialog.dialog({
               modal: true,
               width: 450,
+              dialogClass: "asu-application-confirm-dialog",
+              open: function () {
+                decorateDialogButtons(this);
+              },
               buttons: {
                 [continueLabel]: function () {
                   $(this).dialog("close");
